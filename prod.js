e07899d5e3611706654e44efc738a5180a939b09
Scott Davis <scott@thirstyhead.com>
2013-08-22 16:24:14 -0600

    changed hero-unit to jumbotron

--- prod.js
+++ prod.js
@@ -1,8 +1,9 @@
 var connect = require("connect");
 var port = process.env.PORT || 8000;
 
-connect(
-  connect.static(__dirname + "/www-built")
-).listen(port);
+connect()
+  .use(connect.compress())
+  .use(connect.static(__dirname + "/www-built"))  
+  .listen(port);
 
 console.log("Server listening on port " + port);
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var connect = require("connect");
 var port = 8000;
 
-connect(
-  connect.static(__dirname + "/www")
-).listen(port);
+connect()
+  .use(connect.compress())
+  .use(connect.static(__dirname + "/www"))  
+  .listen(port);
 
 console.log("Server listening on port " + port);
